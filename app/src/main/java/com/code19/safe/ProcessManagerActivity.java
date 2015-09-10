7497fa0faeb6d1f301ada8d53e1d704bae5c46ba
h4de5ing <moxi1992@gmail.com>
2015-09-11 01:40:47 +0800

    修改一个文件，测试能否提交成功

--- app/src/main/java/com/code19/safe/ProcessManagerActivity.java
+++ app/src/main/java/com/code19/safe/ProcessManagerActivity.java
@@ -37,8 +37,6 @@ public class ProcessManagerActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.process_activity);
-
-
         initView();
         initData();
         initEvent();
