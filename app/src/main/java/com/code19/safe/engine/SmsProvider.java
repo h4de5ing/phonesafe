717ae3675a59f64ccff69db5e07752175f643e4e
h4de5ing <moxi1992@gmail.com>
2015-09-11 23:13:21 +0800

    备份短信和还原短信的方法

--- app/src/main/java/com/code19/safe/engine/SmsProvider.java
+++ app/src/main/java/com/code19/safe/engine/SmsProvider.java
@@ -1,16 +1,24 @@
 package com.code19.safe.engine;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.os.SystemClock;
+import android.util.Log;
 
 import com.smartandroid.sa.json.Gson;
+import com.smartandroid.sa.json.reflect.TypeToken;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,7 +35,8 @@ public class SmsProvider {
      * @param context  上下文
      * @param listener 监听器
      */
-    private static void SmsBackup(final Context context, final OnBackupListener listener) {
+    public static void SmsBackup(final Context context, final OnBackupListener listener) {
+        //耗时操作用异步任务
         new AsyncTask<Void, Integer, Boolean>() {
             @Override
             protected Boolean doInBackground(Void... params) {
@@ -38,9 +47,9 @@ public class SmsProvider {
                     List<SmsBean> list = new ArrayList<SmsBean>();
                     if (cursor != null) {
                         int smsCount = cursor.getColumnCount();//短信的总数
-                        SmsBean bean = new SmsBean(); //用于存储短信的ben对象
                         int progress = 0; //进度，保存的当前短信是第几条
                         while (cursor.moveToNext()) {
+                            SmsBean bean = new SmsBean(); //用于存储短信的ben对象
                             bean.address = cursor.getString(0);
                             bean.date = Long.valueOf(cursor.getString(1));
                             bean.type = cursor.getInt(2);
@@ -48,6 +57,7 @@ public class SmsProvider {
                             list.add(bean);
                             progress++;
                             publishProgress(progress, smsCount);//将进度条的当前进度和进度max值实时传给主线程
+                            SystemClock.sleep(300);
                         }
                         cursor.close();
                     }
@@ -58,10 +68,10 @@ public class SmsProvider {
                     FileOutputStream fos = new FileOutputStream(file);
                     fos.write(json.getBytes());
                     fos.close();
-                    return true; //走到这而备份成功
+                    return true; //备份成功，返回ture
                 } catch (Exception e) {
-                    //e.printStackTrace();
-                    return false;//走到这儿备份失败
+                    e.printStackTrace();
+                    return false;//备份失败，返回false
                 }
             }
 
@@ -83,32 +93,95 @@ public class SmsProvider {
         }.execute();
     }
 
-    //new TypeToken<List<SmsBean>>().getType().getClass();
 
     /**
      * 还原短信
      *
      * @param context 上下文
      */
-    public static void SmsRestore(Context context, OnREstoreListener listener) {
+    //new TypeToken<List<SmsBean>>().getType().getClass();
+    public static void SmsRestore(final Context context, final OnRestoreListener listener) {
+        new AsyncTask<Void, Integer, Boolean>() {
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                try {
+                    //1.读取文件
+                    File file = new File(Environment.getExternalStorageDirectory(), "SmsBackup.json");
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    String json = reader.readLine();
+                    reader.close();
+                    Log.i(TAG, "恢复的短信是：" + json);
+                    Gson gson = new Gson();
+                    List<SmsBean> list = gson.fromJson(json, new TypeToken<List<SmsBean>>() {
+                    }.getType());
+                    //将集合插入到数据库
+                    Uri uri = Uri.parse("content://sms");
+                    ContentResolver resolver = context.getContentResolver();
+                    int count = list.size();//短信条数就是进度的最大值
+                    int progress = 0;
+                    for (int i = 0; i < list.size(); i++) {
+                        SmsBean bean = list.get(i);
+                        ContentValues values = new ContentValues();
+                        values.put("address", bean.address);
+                        values.put("date", bean.date);
+                        values.put("type", bean.type);
+                        values.put("body", bean.body);
+                        resolver.insert(uri, values);
+                        progress++;
+                        publishProgress(progress, count);//将当前进度和进度最大值传回
+                    }
+                    return true;
+                } catch (FileNotFoundException e) {
+                    Log.i(TAG, "文件没有找到");
+                    e.printStackTrace();
+                    return false;
+                } catch (IOException e) {
+                    Log.i(TAG, "IO流异常");
+                    e.printStackTrace();
+                    return false;
+                }
+            }
 
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                //拿到进度
+                if (listener != null) {
+                    listener.onProgress(values[0], values[1]);
+                }
+            }
+
+            @Override
+            protected void onPostExecute(Boolean result) {
+                if (listener != null) {
+                    listener.onResult(result);
+                }
+
+
+            }
+        }.execute();
     }
 
 
-}
+    public interface OnBackupListener {
+        //当进度运行时的回调
+        void onProgress(int progress, int max);
 
-interface OnBackupListener {
-    void onProgress(int progress, int max);
+        //当结束时的回调
+        void onResult(boolean result);
+    }
 
-    void onResult(boolean result);
-}
+    public interface OnRestoreListener {
+        //当进度运行时回调
+        void onProgress(int progress, int max);
 
-interface OnREstoreListener {
-}
+        //当结果是回调
+        void onResult(Boolean result);
+    }
 
-class SmsBean {
-    public String address;
-    public long date;
-    public int type;
-    public String body;
+    private static class SmsBean {
+        public String address;
+        public long date;
+        public int type;
+        public String body;
+    }
 }
