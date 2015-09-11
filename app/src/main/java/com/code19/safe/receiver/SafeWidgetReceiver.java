3f706bc639983349a0a4df0b06645e1a4c0dae08
h4de5ing <moxi1992@gmail.com>
2015-09-11 10:51:13 +0800

    增加widget组件的广播接收者

---
+++ app/src/main/java/com/code19/safe/receiver/SafeWidgetReceiver.java
@@ -0,0 +1,11 @@
+package com.code19.safe.receiver;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Gh0st on 2015/9/11.
+ * 10:39
+ */
+public class SafeWidgetReceiver extends AppWidgetProvider {
+    
+}
