41bde6c633cb40d3192da01f5a8819fbf7aed071
h4de5ing <moxi1992@gmail.com>
2015-09-11 23:15:16 +0800

    进程管理主界面和工具界面

--- app/src/main/java/com/code19/safe/ProcessManagerActivity.java
+++ app/src/main/java/com/code19/safe/ProcessManagerActivity.java
@@ -36,7 +36,7 @@ public class ProcessManagerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.process_activity);
+        setContentView(R.layout.activity_process);
         initView();
         initData();
         initEvent();
