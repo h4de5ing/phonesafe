398b36b355ac1027d4edba747406929abcda1e2a
h4de5ing <moxi1992@gmail.com>
2015-09-13 15:20:08 +0800

    手机杀毒功能

---
+++ app/src/main/java/com/code19/safe/VirusCleanManagerActivity.java
@@ -0,0 +1,17 @@
+package com.code19.safe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Gh0st on 2015/9/13.
+ * 0:16
+ */
+public class VirusCleanManagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_virusclean);
+    }
+}
