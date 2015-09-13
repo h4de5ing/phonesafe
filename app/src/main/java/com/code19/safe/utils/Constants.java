20b4e6fa93cc8fe8f3b3f53073942ae17cea0c59
h4de5ing <moxi1992@gmail.com>
2015-09-13 23:21:39 +0800

    常量类

--- app/src/main/java/com/code19/safe/utils/Constants.java
+++ app/src/main/java/com/code19/safe/utils/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final String SAFE_NUMBER = "safe_number";//安全号码
     public static final String PROTECTING = "protecting"; //防盗保护启动状态
     public static final java.lang.String AUTO_UPDATE = "auto_update";//自动更新
+    public static final String NUMBER_ADDRESS_STYLE = "number_address_style"; //号码归属地样式
     public static String SJFD_PwD = "sjfd_pwd"; //手机防盗密码
 
 }
