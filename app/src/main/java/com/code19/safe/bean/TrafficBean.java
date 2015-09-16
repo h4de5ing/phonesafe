fea66c63a1dcc043c2e885b1d27ff0837a69c285
h4de5ing <moxi1992@gmail.com>
2015-09-16 18:19:16 +0800

    流量监控显示的元素的bean类

---
+++ app/src/main/java/com/code19/safe/bean/TrafficBean.java
@@ -0,0 +1,15 @@
+package com.code19.safe.bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Gh0st on 2015/9/16.
+ * 14:00
+ */
+public class TrafficBean {
+    public Drawable icon; //应用图标
+    public String name; //应用名称
+    public String rcv; //发送的数据
+    public String snd; //收到的数据
+
+}
